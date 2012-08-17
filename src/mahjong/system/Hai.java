7966462181be5fc52cfa165de05bc8c7dce3dd22
moseshi <moseshi@gmail.com>
2012-08-18 03:54:33 +0900

    Signed-off-by: moseshi <moseshi@gmail.com>

--- src/mahjong/system/Hai.java
+++ src/mahjong/system/Hai.java
@@ -3,12 +3,16 @@ package mahjong.system;
 public class Hai {
 	boolean aka;
 	HaiType type;
-	int a;
 	
 	public Hai(HaiType type, boolean aka){
 		this.type = type;
 		this.aka = aka;
 	}
+	
+	public void sss(){
+		aka = true;
+	}
+	
 	
 	public HaiType getType(){		
 		return type;
